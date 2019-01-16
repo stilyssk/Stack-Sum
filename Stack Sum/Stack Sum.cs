45604ae30e5cf1d3a52324c944fce18d8e7bb73c
stilyssk <41970026+stilyssk@users.noreply.github.com>
2019-01-16 22:29:33 +0200

    new file:   .vs/Stack Sum/v15/.suo 	new file:   .vs/Stack Sum/v15/Server/sqlite3/db.lock 	new file:   .vs/Stack Sum/v15/Server/sqlite3/storage.ide 	new file:   .vs/Stack Sum/v15/Server/sqlite3/storage.ide-shm 	new file:   .vs/Stack Sum/v15/Server/sqlite3/storage.ide-wal 	new file:   Simple Calculator/Simple Calculator .cs 	new file:   Simple Calculator/Simple Calculator.csproj 	new file:   Simple Calculator/obj/Debug/netcoreapp2.1/Simple Calculator.AssemblyInfo.cs 	new file:   Simple Calculator/obj/Debug/netcoreapp2.1/Simple Calculator.AssemblyInfoInputs.cache 	new file:   Simple Calculator/obj/Debug/netcoreapp2.1/Simple Calculator.assets.cache 	new file:   Simple Calculator/obj/Debug/netcoreapp2.1/Simple Calculator.csproj.CoreCompileInputs.cache 	new file:   Simple Calculator/obj/Debug/netcoreapp2.1/Simple Calculator.csprojAssemblyReference.cache 	new file:   Simple Calculator/obj/Debug/netcoreapp2.1/TemporaryGeneratedFile_036C0B5B-1481-4323-8D20-8F5ADCB23D92.cs 	new file:   Simple Calculator/obj/Debug/netcoreapp2.1/TemporaryGeneratedFile_5937a670-0e60-4077-877b-f7221da3dda1.cs 	new file:   Simple Calculator/obj/Debug/netcoreapp2.1/TemporaryGeneratedFile_E7A71F73-0F8D-4B9B-B56E-8E70B10BC5D3.cs 	new file:   Simple Calculator/obj/Simple Calculator.csproj.nuget.cache 	new file:   Simple Calculator/obj/Simple Calculator.csproj.nuget.g.props 	new file:   Simple Calculator/obj/Simple Calculator.csproj.nuget.g.targets 	new file:   Simple Calculator/obj/project.assets.json 	new file:   Stack Sum.sln 	new file:   Stack Sum/Stack Sum.cs 	new file:   Stack Sum/Stack Sum.csproj 	new file:   Stack Sum/bin/Debug/netcoreapp2.1/Stack Sum.deps.json 	new file:   Stack Sum/bin/Debug/netcoreapp2.1/Stack Sum.dll 	new file:   Stack Sum/bin/Debug/netcoreapp2.1/Stack Sum.pdb 	new file:   Stack Sum/bin/Debug/netcoreapp2.1/Stack Sum.runtimeconfig.dev.json 	new file:   Stack Sum/bin/Debug/netcoreapp2.1/Stack Sum.runtimeconfig.json 	new file:   Stack Sum/obj/Debug/netcoreapp2.1/Stack Sum.AssemblyInfo.cs 	new file:   Stack Sum/obj/Debug/netcoreapp2.1/Stack Sum.AssemblyInfoInputs.cache 	new file:   Stack Sum/obj/Debug/netcoreapp2.1/Stack Sum.assets.cache 	new file:   Stack Sum/obj/Debug/netcoreapp2.1/Stack Sum.csproj.CoreCompileInputs.cache 	new file:   Stack Sum/obj/Debug/netcoreapp2.1/Stack Sum.csproj.FileListAbsolute.txt 	new file:   Stack Sum/obj/Debug/netcoreapp2.1/Stack Sum.csprojAssemblyReference.cache 	new file:   Stack Sum/obj/Debug/netcoreapp2.1/Stack Sum.dll 	new file:   Stack Sum/obj/Debug/netcoreapp2.1/Stack Sum.pdb 	new file:   Stack Sum/obj/Debug/netcoreapp2.1/TemporaryGeneratedFile_036C0B5B-1481-4323-8D20-8F5ADCB23D92.cs 	new file:   Stack Sum/obj/Debug/netcoreapp2.1/TemporaryGeneratedFile_5937a670-0e60-4077-877b-f7221da3dda1.cs 	new file:   Stack Sum/obj/Debug/netcoreapp2.1/TemporaryGeneratedFile_E7A71F73-0F8D-4B9B-B56E-8E70B10BC5D3.cs 	new file:   Stack Sum/obj/Stack Sum.csproj.nuget.cache#	new file:   Stack Sum/obj/Stack Sum.csproj.nuget.g.props 	new file:   Stack Sum/obj/Stack Sum.csproj.nuget.g.targets 	new file:   Stack Sum/obj/project.assets.json

---
+++ Stack Sum/Stack Sum.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Stack_Sum
+{
+    class Program
+    {
+        static  Stack<int> Add(Stack<int> tempStack, int firstNumber, int secondNumber)
+        {
+            tempStack.Push(firstNumber);
+            tempStack.Push(secondNumber);
+            return tempStack;
+        }
+        static Stack<int> Remove(Stack<int> tempStack, int countToRemove)
+        {
+            if (tempStack.Count>=countToRemove)
+            {
+                for (int i = 0; i < countToRemove; i++)
+                {
+                    tempStack.Pop();
+                } 
+            }
+            return tempStack;
+        }
+        static void Main(string[] args)
+        {
+            string inputString = Console.ReadLine();
+            int[] inputNumrerArray = inputString.Split(new char[] { ' ' },StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Stack<int> stackCollection = new Stack<int>();
+            foreach (var numberItem in inputNumrerArray)
+            {
+                stackCollection.Push(numberItem);
+            }
+            while (true)
+            {
+                string[] inputCommand = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.None ).ToArray();
+                string command = inputCommand[0].ToLower();
+                if (command=="end")
+                {
+                    break;
+
+                }
+                int firstNumber;
+
+                switch (command)
+                {
+                    case "add":
+                        firstNumber = int.Parse(inputCommand[1]);
+                        int secondNumber = int.Parse(inputCommand[2]);
+                        stackCollection = Add(stackCollection, firstNumber, secondNumber);
+                        break;
+                    case "remove":
+                        firstNumber = int.Parse(inputCommand[1]);
+                        stackCollection = Remove(stackCollection, firstNumber);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+            int sum = 0;
+            while (stackCollection.Any())
+            {
+
+                sum += stackCollection.Pop();
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
